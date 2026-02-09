21aae0d9ad012458979b275d14700aaed71e894d
JJw <jakesjordan00@gmail.com>
2026-02-09 17:01:53 -0500

    moved the transform testing code over to the transform_playbyplay file and ConsolidateSOurces function

--- Refactor/pipelines/playbyplay.py
+++ Refactor/pipelines/playbyplay.py
@@ -26,88 +26,13 @@ class PlayByPlayPipeline(Pipeline[dict]):
         }
 
     def extract(self):
-        data_extract = self.source.fetch()
+        static_data_extract = self.source.fetch()
         api_data_extract = self.api_source.fetch()
         extract = {
-            'data_extract': data_extract,
+            'static_data_extract': static_data_extract,
             'api_data_extract': api_data_extract
         }
-
-        tde = data_extract['game']['actions']
-        tad = api_data_extract['game']['actions']
-
-        test_tde = pd.DataFrame(tde)
-        test_tad = pd.DataFrame(tad)
-        test_tad['actionType'] = test_tad['actionType'].str.lower()
-
-
-        test_left = test_tde.merge(test_tad[['actionType', 'personId', 'clock', 'period', 'description', 'actionNumber']], on=['actionType', 'personId', 'clock', 'period'], how='left', suffixes=('', '_api'))
-                
-
-        mask_out = (test_left['actionType'] == 'substitution')
-        sub_out = test_left[[
-            'actionNumber','period','clock', 
-            'personId', 'playerName', 'playerNameI', 
-            'description', 'description_api',
-            'actionType', 'subType',
-            'scoreHome', 'scoreAway',
-        ]][mask_out]
-
-        sub_out['description_api'] = sub_out['description_api'].fillna('')
-        sub_out['description_sub_in'] = sub_out.apply(
-            lambda row: (
-                row['description_api']
-                .replace(':', ' in:')
-                .replace(' FOR ', '')
-                .replace(row['playerName'], '')
-            ), 
-            axis=1
-        )        
         
-        mask_in = (test_left['actionType'] == 'substitution') & (test_left['subType'] == 'in')
-        sub_in = test_left[[
-            'actionNumber','period','clock', 
-            'personId', 'playerName', 'playerNameI', 
-            'description',
-            'actionType', 'subType',
-            'scoreHome', 'scoreAway',
-        ]][mask_in]
-        sub_in['description_sub_in'] = sub_in.apply(
-            lambda row:(
-            row['description'].replace(row['playerNameI'], row['playerName'])
-            ),
-            axis = 1
-        )
-
-
-        combined = sub_out.merge(sub_in, how='left', on = ['period','clock', 'scoreHome', 'scoreAway', 'actionType', 'description_sub_in']
-                                 , suffixes=('_dfOut', '_dfIn'))
-        combined['description_dfIn'] = combined['description_dfIn'].fillna('')
-
-        print(f'Sub out: {sub_out['description_sub_in'].iloc[0]}')
-        print(f'Sub in:  {sub_in['description_sub_in'].iloc[0]}')
-
-
-        matched_sub_in = combined[combined['actionNumber_dfIn'].notna()]['actionNumber_dfIn'].values
-        combined = combined[~(
-            (combined['subType_dfOut'] == 'in') & 
-            (combined['actionNumber_dfIn'].isna()) & 
-            (combined['actionNumber_dfOut'].isin(matched_sub_in))
-        )]
-
-        bp = 'here'
-        for i, row in combined.iterrows():
-            if row['actionType'] == 'substitution':
-                print(f'{row['actionNumber_dfOut']} - {row['description_dfOut']}'
-                      + f'{(35 - len(row['description_dfOut'])) * ' '}'
-                      + f'{row['actionNumber_dfIn']} - {row['description_dfIn']}'
-                      + f'{(35 - len(row['description_dfIn'])) * ' '}'
-                      + f'API: {row['description_api']}')
-                
-
-
-        bp = 'here'
-
         return extract
 
 
--- Refactor/transforms/transform_playbyplay.py
+++ Refactor/transforms/transform_playbyplay.py
@@ -1,5 +1,6 @@
 import config.data_map
-
+import pandas as pd
+import polars as pl
 
 
 class Transform:
@@ -9,15 +10,100 @@ class Transform:
         pass
 
     def playbyplay(self, data_extract_full):
-        data_extract = data_extract_full['data_extract']
-        api_data_extract = data_extract_full['api_data_extract']
-        playbyplay_data = data_extract['game']['actions']
-        api_playbyplay_data = api_data_extract['game']['actions']
+        playbyplay_data = ConsolidateSources(data_extract_full)
         scoreboard_data = self.pipeline.Data['scoreboard_data']
         boxscore_data = self.pipeline.Data['boxscore_data']
         start_action = self.pipeline.start_action
         transformed_playbyplay = TransformPlayByPlay(playbyplay_data, scoreboard_data, boxscore_data, start_action)
         return transformed_playbyplay
+
+def ConsolidateSources(data_extract_full: dict):
+    static_data_extract = data_extract_full['static_data_extract']
+    api_data_extract = data_extract_full['api_data_extract']
+    static_playbyplay_data = static_data_extract['game']['actions']
+    api_playbyplay_data = api_data_extract['game']['actions']
+
+    tde = static_data_extract['game']['actions']
+    tad = api_data_extract['game']['actions']
+
+    test_tde = pd.DataFrame(tde)
+    test_tad = pd.DataFrame(tad)
+    test_tad['actionType'] = test_tad['actionType'].str.lower()
+
+
+    test_left = test_tde.merge(test_tad[['actionType', 'personId', 'clock', 'period', 'description', 'actionNumber']], on=['actionType', 'personId', 'clock', 'period'], how='left', suffixes=('', '_api'))
+            
+
+    mask_out = (test_left['actionType'] == 'substitution')
+    sub_out = test_left[[
+        'actionNumber','period','clock', 
+        'personId', 'playerName', 'playerNameI', 
+        'description', 'description_api',
+        'actionType', 'subType',
+        'scoreHome', 'scoreAway',
+    ]][mask_out]
+
+    sub_out['description_api'] = sub_out['description_api'].fillna('')
+    sub_out['description_sub_in'] = sub_out.apply(
+        lambda row: (
+            row['description_api']
+            .replace(':', ' in:')
+            .replace(' FOR ', '')
+            .replace(row['playerName'], '')
+        ), 
+        axis=1
+    )        
+    
+    mask_in = (test_left['actionType'] == 'substitution') & (test_left['subType'] == 'in')
+    sub_in = test_left[[
+        'actionNumber','period','clock', 
+        'personId', 'playerName', 'playerNameI', 
+        'description',
+        'actionType', 'subType',
+        'scoreHome', 'scoreAway',
+    ]][mask_in]
+    sub_in['description_sub_in'] = sub_in.apply(
+        lambda row:(
+        row['description'].replace(row['playerNameI'], row['playerName'])
+        ),
+        axis = 1
+    )
+
+
+    combined = sub_out.merge(sub_in, how='left', on = ['period','clock', 'scoreHome', 'scoreAway', 'actionType', 'description_sub_in']
+                                , suffixes=('_dfOut', '_dfIn'))
+    combined['description_dfIn'] = combined['description_dfIn'].fillna('')
+
+    print(f'Sub out: {sub_out['description_sub_in'].iloc[0]}')
+    print(f'Sub in:  {sub_in['description_sub_in'].iloc[0]}')
+
+
+    matched_sub_in = combined[combined['actionNumber_dfIn'].notna()]['actionNumber_dfIn'].values
+    combined = combined[~(
+        (combined['subType_dfOut'] == 'in') & 
+        (combined['actionNumber_dfIn'].isna()) & 
+        (combined['actionNumber_dfOut'].isin(matched_sub_in))
+    )]
+
+    bp = 'here'
+    for i, row in combined.iterrows():
+        if row['actionType'] == 'substitution':
+            print(f'{row['actionNumber_dfOut']} - {row['description_dfOut']}'
+                    + f'{(35 - len(row['description_dfOut'])) * ' '}'
+                    + f'{row['actionNumber_dfIn']} - {row['description_dfIn']}'
+                    + f'{(35 - len(row['description_dfIn'])) * ' '}'
+                    + f'API: {row['description_api']}')
+            
+    bp = 'here'
+            
+
+
+
+    playbyplay_data = []
+    return playbyplay_data
+
+
+
 
 
 
