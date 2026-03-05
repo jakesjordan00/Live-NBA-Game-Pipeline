d899e2b7ee4fe5751424de71a619020f85f2328b
JJw <jakesjordan00@gmail.com>
2026-03-05 16:58:34 -0500

    Completing nba_schedule_pipeline

--- dags/nba_schedule_pipeline.py
+++ dags/nba_schedule_pipeline.py
@@ -25,7 +25,7 @@ def nba_pipeline():
         return schedule_data
     
 
-
+    @task
     def run_boxscore(game):
         from pipelines import BoxscorePipeline
         GameID = game['GameID']
@@ -47,10 +47,43 @@ def nba_pipeline():
         stint_status = start_action_info['stint_status']
         
         home_stats, away_stats = (None, None) if db_actions == 0 or stint_status == 'failure' else boxscore_pipeline.destination.stint_cursor(boxscore_data['lineup_keys'])
+        boxscore_data = {
+            'boxscore_data': boxscore_data,
+            'db_actions': db_actions,
+            'db_last_action_number': db_last_action_number,
+            'home_stats': home_stats,
+            'away_stats': away_stats,
+            'stint_status': stint_status
+        }
+        return boxscore_data
 
+    @task
+    def run_playbyplay(boxscore_result):
+        from pipelines import PlayByPlayPipeline
+        box_data = boxscore_result['boxscore_data']
+        GameID = box_data['GameID']
+        playbyplay_pipeline = PlayByPlayPipeline(
+            pipeline_name = f'playbyplay.{GameID}',
+            boxscore_data = box_data,
+            db_actions = box_data['db_actions'],
+            db_last_action_number = box_data['db_last_action_number'],
+            home_stats = box_data['home_stats'],
+            away_stats = box_data['away_stats'],
+            stint_status = box_data['stint_status'],
+            environment = 'Production'
+        )
+        completed_playbyplay_pipeline = playbyplay_pipeline.run()
+        loaded_playbyplay_data = completed_playbyplay_pipeline['loaded']
 
-
+        return {
+            'playbyplay_actions': len(loaded_playbyplay_data['PlayByPlay']),
+            'stint_errors': [asdict(err) for err in loaded_playbyplay_data['Stints'].errors]
+        }
 
     games = run_schedule()
+
+    boxscore_results = run_boxscore.expand(game = games)
+
+    run_playbyplay.expand(boxscore_result = boxscore_results)
 
 nba_pipeline()
